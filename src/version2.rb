10a21aaa237d9e7cffea010950f2df5dd46f5474
VitorVT9 <vsilvab57@gmail.com>
2026-08-03 01:35:45 -0300

    feat: cria sorteio com while sem realizar colisões de números

--- src/version2.rb
+++ src/version2.rb
@@ -2,7 +2,6 @@ puts ""
 puts "--->Através desse programa vamos preencher um vetor de tamanho N com os números de 1 até N."
 puts "-->Sendo feito em ordem pseudoaleatória, impedindo que um número seja armazenado mais de uma vez."
 puts ""
-
 print "Informe o número de indices do vetor: "
 indi_vetor = gets.chomp.to_i
 
@@ -17,7 +16,19 @@ meu_vetor = Array.new(indi_vetor)
 numeros_armazenados = 0
 colisoes = 0 
 total_sorteios = 0
+numeros_disponiveis = (1..indi_vetor).to_a
 tempo_inicio = Time.now
+
+while numeros_armazenados < indi_vetor
+  total_sorteios += 1
+  indice_sorteado = rand(numeros_disponiveis.length)
+  numero_sorteado = numeros_disponiveis[indice_sorteado]
+  
+  meu_vetor[numeros_armazenados] = numero_sorteado
+  numeros_armazenados += 1
+
+  numeros_disponiveis.delete_at(indice_sorteado)
+end
 
 tempo_fim = Time.now
 tempo_execucao = tempo_fim - tempo_inicio
