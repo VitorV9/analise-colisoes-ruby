6b1d79a5536501c01f0e23fc7a2b7da8d727b0c3
VitorVT9 <vsilvab57@gmail.com>
2026-08-03 18:25:33 -0300

    feat: cria arquivo separado para saída dos resultados da V2

---
+++ src/execV2.rb
@@ -0,0 +1,11 @@
+require_relative 'version2'
+require_relative 'validaVetor'
+require_relative 'exibirResult'
+
+puts ""
+puts "---> EXECUTANDO VERSÃO 2 (sem rand e colisões)"
+
+tamanho_n = solicitar_tamanho_vetor()
+meu_vetor, sorteios, armazenados, colisoes, tempo = preencher_vetor_v2(tamanho_n)
+status_validacao = validar_vetor(meu_vetor, tamanho_n)
+exibir_resultados(meu_vetor, sorteios, armazenados, colisoes, tempo, status_validacao)
